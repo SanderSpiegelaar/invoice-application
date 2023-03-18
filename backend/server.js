15f75f29f400f484e9b23c20498a15e43aa7bcd3
Sander Spiegelaar <sanderspiegelaar@gmail.com>
2023-03-18 12:31:01 +0100

    invoice

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cookieParser from 'cookie-parser';
 import "dotenv/config";
 import express from 'express';
 import morgan from 'morgan';
-
+import { morganMiddleware, systemLogs } from './utils/Logger.js';
 
 const app = express();
 
@@ -14,6 +14,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(morganMiddleware);
 
 app.get('/api/v1/test', (req, res) => {
     res.json({Hi: "Welcome to the API"});
@@ -25,4 +26,5 @@ app.listen(PORT, () =>{
     console.log(
         `${chalk.green.bold("✔")} 👍 Server running in ${chalk.yellow.bold(process.env.NODE_ENV)} mode on port ${chalk.blue.bold(PORT)}`
     );
+    systemLogs.info(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the invoice App!!!!2 test 3</h1>
+        </div>
+    )
+}
+
+export default App
